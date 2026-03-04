7af499bcf075c065933e3c7e31e5257d9e135f77
A.Jackson <andrewz.jackson667@gmail.com>
2026-03-04 12:41:20 +0900

    Skeleton components.

---
+++ src/features/landing/Landing.tsx
@@ -0,0 +1,14 @@
+import Hero from './components/Hero'
+import Gallery from './components/Gallery'
+
+
+export default function Landing() {
+
+    return (
+        <div>
+            <h1>Media Library Landing</h1>
+            <Hero />
+            <Gallery />
+        </div>
+    )
+}
