50890b7c243ef4c8223f1d46486e5413f0d23cc1
A.Jackson <andrewz.jackson667@gmail.com>
2026-03-18 02:53:44 +0900

    Added: Footer section.

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,20 @@ import './core/styles/App.css';
 import SideBar from './features/sidebar/SideBar';
 import NavBar from './features/nav/NavBar';
 import Content from './features/content/Content';
+import Footer from './features/footer/Footer';
 
 export default function App() {
 
   return (
     <>
-      <SideBar />
-      <div id='rightSection'>
-        <NavBar />
-        <Content />
-      </div>
+      <main>
+        <SideBar />
+        <div id='rightSection'>
+          <NavBar />
+          <Content />
+        </div>
+      </main>
+      <Footer />
     </>
 
   )
---
+++ src/features/footer/Footer.tsx
@@ -0,0 +1,12 @@
+
+
+
+export default function Footer() {
+
+    return (
+        <footer>
+            <p>Author: Andrew Jackson   |   GitHub: <a href="https://github.com/sonAndrew/Media_Lib_UI">Media Lib UI</a></p>
+
+        </footer>
+    )
+}
